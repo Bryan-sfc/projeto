6b4b5ab41cf66acc0a60927b06cfa9082a954bfe
Bryan Goncalves <goncalvesbryan424@gmail.com>
2024-06-13 16:50:48 -0300

    Projeto

---
+++ Rascunho.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    string opcoes[5];
+    string op;
+
+    do{
+        
+    } while (op == 0);
+
+    system("cls");
+    
+    cout << "Obrigado Por Utilizar O programa :)"
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
